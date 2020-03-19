d810c92feb2e213c7ee3ff0e31a715bc8d962d08
besplatnayahurma <hummr65@gmail.com>
2020-03-19 15:07:53 +0300

    Added sorting test

--- src/test/java/com/dbtask/DbHelperTest.java
+++ src/test/java/com/dbtask/DbHelperTest.java
@@ -2,11 +2,41 @@ package com.dbtask;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.Date;
+import java.util.List;
+import java.util.Spliterator;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class DbHelperTest {
     @Test
-    void getCollection() {
-        assertDoesNotThrow(() -> new DbHelper().getCollection());
+    void collectionIsSortedAscending() {
+        Spliterator<LocalDateTime> spliterator = new DbHelper()
+                .getCollection()
+                .find()
+                .map(document -> (Date) document.get("time"))
+                .map(date -> LocalDateTime.ofInstant(date.toInstant(), ZoneId.systemDefault()))
+                .spliterator();
+
+        List<LocalDateTime> list = StreamSupport
+                .stream(spliterator, false)
+                .collect(Collectors.toList());
+
+        assertTrue(list.size() != 0);
+        assertTrue(isSorted(list));
+    }
+
+    private <T extends Comparable<? super T>> boolean isSorted(List<T> array){
+        for (int i = 0; i < array.size() - 1; i++) {
+            if (array.get(i).compareTo(array.get(i + 1)) > 0) {
+                return false;
+            }
+        }
+
+        return true;
     }
 }
